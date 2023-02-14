aba1f6a1f5ea9e796dcd96c6f39c3cb8722d1cca
kench <kench1994.h.wong@gmai.com>
2023-02-14 09:32:36 +0800

    ::construct::

--- projects/static/xmake.lua
+++ projects/static/xmake.lua
@@ -42,8 +42,16 @@ add_requires( string.format("libcurl %s", libcurl_version), {
     }
 )
 
+target("predeps1")
+    set_kind("binary")
+    add_files("../../src/*.c")
+    add_packages("zlib")
+    set_installdir("$(projectdir)/publish/")
+target_end()
+
 target("test")
     set_kind("binary")
+    add_deps("predeps1")
     add_files("../../src/*.c")
     -- 主要为了 hook install、uninstall 动作
     add_packages("libcurl", "openssl", "zlib")
